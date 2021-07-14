a97d8c10a4f5e7ed13897d16fdb52aa098aa2bee
thongchai <thongchai.sh@gmail.com>
2021-07-14 21:55:23 +0700

    initial project add . closes #1

---
+++ src/test/java/com/ith/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ith.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
