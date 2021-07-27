a1e9bb774c979b3d77d65e815f9d7364e2ff4daf
thongchai <thongchai.sh@gmail.com>
2021-07-27 11:30:07 +0700

    Adding methods for controller. closes #16

---
+++ src/main/java/com/ith/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,46 @@
+package com.ith.msscbeerservice.services;
+
+import com.ith.msscbeerservice.domain.Beer;
+import com.ith.msscbeerservice.repositories.BeerRepository;
+import com.ith.msscbeerservice.web.controller.NotFoundException;
+import com.ith.msscbeerservice.web.mappers.BeerMapper;
+import com.ith.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getById(UUID beerId) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
+        );
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.save(beerMapper.beerDtoToBeer(beerDto))
+        );
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        Beer beer=beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle().name());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUpc(beerDto.getUpc());
+        return beerMapper.beerToBeerDto(
+                beerRepository.save(beer)
+        );
+    }
+}
---
+++ src/main/java/com/ith/msscbeerservice/web/controller/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.ith.msscbeerservice.web.controller;
+
+public class NotFoundException extends RuntimeException{
+}
