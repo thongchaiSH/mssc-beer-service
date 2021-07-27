8f279360204b240135e8cf86de8fe94b15fcdd9f
thongchai <thongchai.sh@gmail.com>
2021-07-27 11:07:23 +0700

    Adding Jackson config for beerDto. closes #14

--- src/main/java/com/ith/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/ith/msscbeerservice/web/model/BeerDto.java
@@ -21,7 +21,7 @@ public class BeerDto {
 
     @Null
     private Integer version;
-    
+
     @Null
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
